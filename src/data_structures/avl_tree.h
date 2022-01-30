f37e5728d05dbec67b7cabb4406380ef70289af3
dmitry-vorobiev <25718561+dmitry-vorobiev@users.noreply.github.com>
2022-01-30 12:34:33 +0300

    avl: ref

--- src/data_structures/avl_tree.h
+++ src/data_structures/avl_tree.h
@@ -2,124 +2,115 @@
  *https://habr.com/ru/post/150732/ 
  */
 #include <cassert>
+#include <cmath>
 #include <stdint.h>
 #include <type_traits>
 #include <iostream>
 
-template <typename T, typename std::enable_if<std::is_arithmetic<T>::value>::type* = nullptr>
+namespace avl_tree {
+
+template <typename K, typename std::enable_if<std::is_arithmetic<K>::value>::type* = nullptr>
 struct Node
 {
-	T key;
-	uint8_t height;
-	Node<T>* left;
-	Node<T>* right;
+	K key;
+	uint8_t h;
+	Node<K>* left;
+	Node<K>* right;
 
-	Node(T k) : 
-        key(k)
-    {
-        left = nullptr;
-        right = nullptr; 
-        height = 1;
-    }
+    Node(K k) noexcept { key = k; left = right = nullptr; h = 1; }
+
+	static uint8_t height(Node<K>* p) {	return p ? p->h : 0; }
+
+	static int8_t bfactor(Node<K>* p) {	return height(p->right) - height(p->left); }
+
+	static Node<K>* insert_new_node(Node<K>* p, K k)
+	{
+		if (!p) return new Node<K>(k);
+
+		if (k < p->key)
+			p->left = insert_new_node(p->left, k);
+		else
+			p->right = insert_new_node(p->right, k);
+		
+		return balance(p);
+	}
+
+	static Node<K>* rotate_right(Node<K>* p)
+	{
+		Node<K>* q = p->left;
+		p->left = q->right;
+		q->right = p;
+		p->fix_height();
+		q->fix_height();
+		return q;
+	}
+
+	static Node<K>* rotate_left(Node<K>* q)
+	{
+		Node<K>* p = q->right;
+		q->right = p->left;
+		p->left = q;
+		q->fix_height();
+		p->fix_height();
+		return p;
+	}
+
+	static Node<K>* balance(Node<K>* p)
+	{
+		p->fix_height();
+
+		auto bf_p = bfactor(p);
+		assert(-3 < bf_p && bf_p < 3);
+
+		if (bf_p == 2)
+		{
+			if (bfactor(p->right) < 0)
+			{
+				p->right = rotate_right(p->right);
+			} 
+			return rotate_left(p);
+		}
+		if (bf_p == -2)
+		{
+			if (bfactor(p->left) > 0)
+			{
+				p->left = rotate_left(p->left);
+			}
+			return rotate_right(p);
+		}
+		return p;
+	}
+
+	void fix_height()
+	{
+		uint8_t h_l = height(this->left);
+		uint8_t h_r = height(this->right);
+		this->h = (h_l > h_r ? h_l : h_r) + 1;
+	}
 };
 
-template <typename T>
-uint8_t height(Node<T>* p)
-{
-	return p ? p->height : 0;
-}
-
-template <typename T>
-void fix_height(Node<T>* p)
-{
-	auto h_l = height(p->left);
-	auto h_r = height(p->right);
-	p->height = (h_l > h_r ? h_l : h_r) + 1;
-}
-
-template <typename T>
-Node<T>* rotate_right(Node<T>* p)
-{
-	Node<T>* q = p->left;
-	p->left = q->right;
-	q->right = p;
-	fix_height(p);
-	fix_height(q);
-	return q;
-}
-
-template <typename T>
-Node<T>* rotate_left(Node<T>* q)
-{
-	Node<T>* p = q->right;
-	q->right = p->left;
-	p->left = q;
-	fix_height(q);
-	fix_height(p);
-	return p;
-}
-
-template <typename T>
-int8_t bfactor(Node<T>* p)
-{
-	return height(p->right) - height(p->left);
-}
-
-template <typename T>
-Node<T>* balance(Node<T>* p)
-{
-	fix_height(p);
-
-    auto bf_p = bfactor(p);
-    assert(-3 < bf_p && bf_p < 3);
-
-	if (bf_p == 2)
-	{
-		if (bfactor(p->right) < 0)
-        {
-            p->right = rotate_right(p->right);
-        } 
-		return rotate_left(p);
-	}
-	if (bf_p == -2)
-	{
-		if (bfactor(p->left) > 0)
-        {
-			p->left = rotate_left(p->left);
-        }
-		return rotate_right(p);
-	}
-	return p;
-}
-
-template <typename T>
-Node<T>* insert_new_node(Node<T>* p, T k)
-{
-	if (!p) 
-        return new Node<T>(k);
-
-	if (k < p->key)
-		p->left = insert_new_node(p->left, k);
-	else
-		p->right = insert_new_node(p->right, k);
-    
-	return balance(p);
-}
-
-template <typename T, typename std::enable_if<std::is_arithmetic<T>::value>::type* = nullptr>
-class AVLTree
+template <typename K, typename std::enable_if<std::is_arithmetic<K>::value>::type* = nullptr>
+class Tree
 {
 private:
-    Node<T>* m_root;
+    Node<K>* m_root;
+	uint32_t m_size;
     
 public:
-    AVLTree() { m_root = nullptr; };
-    ~AVLTree() {};
+    Tree() { m_root = nullptr; m_size = 0; };
+    ~Tree() {};
 
-    uint8_t height() { return m_root->height; }
+    uint8_t height() const { return Node<K>::height(m_root); }
 
-    void insert(T k) { m_root = insert_new_node<T>(m_root, k); }
+	uint32_t size() const { return m_size; }
+
+    void insert(K k) { 
+		m_root = Node<K>::insert_new_node(m_root, k);
+		m_size++;
+	}
 };
+
+}
 
 bool test_avl_tree()
 {
@@ -131,13 +122,19 @@ bool test_avl_tree()
         29, -35, 45, 19, -8, 22, 33, 50
     };
 
-    AVLTree<int> tree;
+    avl_tree::Tree<int> tree;
 
     for (auto value : test_values)
     {
         tree.insert(value);
+
+		float n = static_cast<float>(tree.size());
+		float h = static_cast<float>(tree.height());
+		assert(std::log2f(n + 1.f) <= h);
+		assert(h <= 1.44f * std::log2f(n + 2.f) - 0.328f);
     }
 
+	std::cout << "Size: " << tree.size() << std::endl;
     std::cout << "Height: " << static_cast<int>(tree.height()) << std::endl;
     return true;
 }
